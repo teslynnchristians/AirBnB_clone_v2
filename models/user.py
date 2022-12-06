4e8b494f9871af89247864d8dc4f16131e0d80e5
teslynnchristians <104847651+teslynnchristians@users.noreply.github.com>
2022-12-06 14:41:19 +0200

    AirBnB clone - MySQL

--- models/user.py
+++ models/user.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-
-"""
-A module that defines the ORM class for User table
-"""
-from os import getenv
-from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String
+"""This module defines a class User"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """
-    Defines attributes for User table
-    """
-    __tablename__ = 'users'
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship(
-            'Place', backref='user', cascade='all, delete')
-        reviews = relationship(
-            'Review', backref='user', cascade='all, delete')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    """This class defines a user by various attributes"""
+    __tablename__ = "users"
+    email = Column('email', String(128), nullable=False)
+    password = Column('password', String(128), nullable=False)
+    first_name = Column('first_name', String(128), nullable=True, default="NULL")
+    last_name = Column('last_name', String(128), nullable=True, default="NULL")
+    # backref may need to be back_populates?Below line commented out bc console
+    # would not run with it in. This line was implemented in Task 8
+    places = relationship("Place", cascade="delete", backref="user")
+    # Below line is commented out for caution and was added in Task 9
+    reviews = relationship("Review", cascade="delete", backref="user")
