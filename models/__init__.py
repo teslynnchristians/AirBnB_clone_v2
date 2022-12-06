67a86bc5903767a37289f60ec8c0848068f9d792
teslynnchristians <104847651+teslynnchristians@users.noreply.github.com>
2022-12-06 14:18:55 +0200

    AirBnB clone - MySQL

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""This module instantiates storage object
+@TODOS:
+    checks HBNB_TYPE_STORAGE environmental variable to determine storage type
+"""
+from os import getenv
+from .user import User
+from .city import City
+from .place import Place
+from .state import State
+from .review import Review
+from .amenity import Amenity
 
-
-storage = FileStorage()
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
