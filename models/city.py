fce772068b63735191d17d1813795c48d2f491a1
teslynnchristians <104847651+teslynnchristians@users.noreply.github.com>
2022-12-06 14:48:51 +0200

    AirBnB clone - MySQL

--- models/city.py
+++ models/city.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
-
-"""
-A module that defines the ORM class for City table
-"""
-from os import getenv
+""" City Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.base_model import Base, BaseModel
-from sqlalchemy import Column, ForeignKey, String
 
 
 class City(BaseModel, Base):
-    """
-    The city class, contains state ID and name
-    """
+    """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship(
-            'Place', backref='cities', cascade='all, delete, delete-orphan')
-    else:
-        name = ''
-        state_id = ''
+    state_id = Column(String(60), ForeignKey('states.id'),
+                      nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", cascade="delete", backref="cities")
