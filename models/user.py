749ac779e9f053055f35accdc9f5362beda13823
teslynnchristians <104847651+teslynnchristians@users.noreply.github.com>
2022-12-06 14:25:56 +0200

    AirBnB clone - MySQL

--- models/user.py
+++ models/user.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
-from models.base_model import BaseModel, Base
+
+"""
+A module that defines the ORM class for User table
+"""
 from os import getenv
-import sqlalchemy
+from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
+    """
+    Defines attributes for User table
+    """
+    __tablename__ = 'users'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        places = relationship(
+            'Place', backref='user', cascade='all, delete')
+        reviews = relationship(
+            'Review', backref='user', cascade='all, delete')
     else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
