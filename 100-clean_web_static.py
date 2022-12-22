9a510f6b540da0f7ec6651728020b04be2e578f4
teslynnchristians <104847651+teslynnchristians@users.noreply.github.com>
2022-12-22 11:31:26 +0200

    AirBnB clone - Deploy static

---
+++ 100-clean_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Function that deploys """
+from fabric.api import local, run, env
+
+
+env.hosts = ["3.95.133.255", "54.87.207.29"]
+env.user = "ubuntu"
+
+
+def do_clean(number=0):
+    """ CLEANS """
+
+    number = int(number)
+
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
